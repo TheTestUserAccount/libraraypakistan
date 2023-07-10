2a2c4104733347f06480b584c60670a93c6dae17
Haider Qadir <48542399+HaiderQadir@users.noreply.github.com>
2023-07-10 09:46:03 +0500

    committed

--- app/src/main/java/com/example/ratingdialog/MainActivity.kt
+++ app/src/main/java/com/example/ratingdialog/MainActivity.kt
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
-import com.example.mainlibrary.hello
+
+//import com.example.mainlibrary.hello
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var hello=hello()
+//        var hello=hello()
 
 //        var newFRag = RatingDialog(RatingModel("Kindly rate your experience with our Sales ",
 //            "Kindly rate your experience with our Sales ",
